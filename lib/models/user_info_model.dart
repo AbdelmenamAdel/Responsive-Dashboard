db2daac70cccb87282ca0b45c3842c03b04a06c7
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-02-15 20:50:46 +0200

    user info model

---
+++ lib/models/user_info_model.dart
@@ -0,0 +1,11 @@
+class UserInfoModel {
+  final String title;
+  final String subtitle;
+  final String asset;
+
+  UserInfoModel({
+    required this.title,
+    required this.subtitle,
+    required this.asset,
+  });
+}
