6476a745fba6bf5522770ad51da674bac4625dd8
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-02-15 20:51:41 +0200

    custom drawer

---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/models/drawer_model.dart';
+import 'package:responsive_dashboard/models/user_info_model.dart';
+import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/widgets/active_and_inactive_drawer_item.dart';
+import 'package:responsive_dashboard/widgets/drawer_items_list_view.dart';
+import 'package:responsive_dashboard/widgets/user_info_list_tile.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width * .7,
+      color: const Color.fromRGBO(255, 255, 255, 1),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+              userInfoModel: UserInfoModel(
+                title: 'Lekan Okeowo',
+                subtitle: 'demo@gmail.com',
+                asset: Assets.imagesAvatar3,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
+          ),
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox(height: 8)),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Setting system', asset: Assets.imagesSettings),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Logout account', asset: Assets.imagesLogout),
+                ),
+                SizedBox(height: 48)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
