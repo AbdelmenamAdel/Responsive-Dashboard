febac7c23b07015868334d52245dad58fed34454
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-04-08 17:59:39 +0200

    dashboard on several views

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,28 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/all_expensess_and_quick_invoice_section.dart';
 import 'package:responsive_dashboard/widgets/custom_drawer.dart';
+import 'package:responsive_dashboard/widgets/income_section.dart';
+import 'package:responsive_dashboard/widgets/my_cards_and_transction_history_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: CustomDrawer(),
+    return Row(
+      children: [
+        Expanded(
+          flex: 3,
+          child: CustomDrawer(),
+        ),
+        SizedBox(width: 16),
+        Expanded(
+          flex: 6,
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: AllExpensessAndQuickInvoiceSection(),
+                ),
+              )
+            ],
           ),
-          Expanded(
-            flex: 6,
-            child: Container(),
+        ),
+        SizedBox(width: 12),
+        Expanded(
+          flex: 4,
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Column(
+                    spacing: 24,
+                    children: [
+                      MyCardsAndTransctionHistorySection(),
+                      Expanded(child: IncomeSection()),
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
-          Expanded(
-            flex: 3,
-            child: Container(),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
