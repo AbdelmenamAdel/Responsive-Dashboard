28e3d67e6de783400726aa3ee6f3f8cbf6d85087
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-04-08 17:59:10 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/dashboard_view.dart';
 
 void main() {
-  runApp(const ResponsiveDashboard());
+  runApp(
+    DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) {
+          return const ResponsiveDashboard();
+        }),
+  );
 }
 
 class ResponsiveDashboard extends StatelessWidget {
@@ -12,8 +20,10 @@ class ResponsiveDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
       debugShowCheckedModeBanner: false,
-      home: DashboardView(),
+      home: DashBoradView(),
     );
   }
 }
