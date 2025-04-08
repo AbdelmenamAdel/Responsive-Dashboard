972a3ffb7348df30c42885486e90d4783d41e375
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-04-08 18:00:03 +0200

    income section is done

---
+++ lib/widgets/income_section_body.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/size_config.dart';
+import 'package:responsive_dashboard/widgets/detailed_income_chart.dart';
+import 'package:responsive_dashboard/widgets/income_chart.dart';
+import 'package:responsive_dashboard/widgets/income_details.dart';
+
+class IncomSectionBody extends StatelessWidget {
+  const IncomSectionBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.sizeOf(context).width;
+    return width >= SizeConfig.desktop && width < 1750
+        ? Expanded(
+            child: Padding(
+            padding: EdgeInsets.all(16),
+            child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: 300,
+                ),
+                child: DetailedIncomeChart()),
+          ))
+        : const Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(child: IncomeChart()),
+              Expanded(flex: 2, child: IncomeDetails()),
+            ],
+          );
+  }
+}
